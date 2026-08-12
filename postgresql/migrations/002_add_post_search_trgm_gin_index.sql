f2ab91936032fb26a4dcf2da8babacb6add61c0d
hawkie275 <hawkie.reb@gmail.com>
2026-08-12 18:49:59 +0900

    Create new search index before dropping old index

--- postgresql/migrations/002_add_post_search_trgm_gin_index.sql
+++ postgresql/migrations/002_add_post_search_trgm_gin_index.sql
@@ -3,7 +3,7 @@
 
 CREATE EXTENSION IF NOT EXISTS pg_trgm;
 
-DROP INDEX IF EXISTS idx_posts_title_content_gin;
-
 CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_posts_search_trgm_gin ON posts
     USING GIN ((coalesce(title, '') || ' ' || coalesce(excerpt, '') || ' ' || coalesce(content, '')) gin_trgm_ops);
+
+DROP INDEX CONCURRENTLY IF EXISTS idx_posts_title_content_gin;
